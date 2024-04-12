2fdb785e3d762a08d7a3eb77086ae92c289b8455
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2024-04-12 15:25:20 +0200

    Implement `AppError` class

---
+++ src/util/appError.ts
@@ -0,0 +1,17 @@
+class AppError extends Error {
+    public statusCode: number;
+    public status: string;
+    public isOperational: boolean;
+
+    constructor(message: string, statusCode: number = 500) {
+        super(message);
+        this.statusCode = statusCode;
+        this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
+        this.isOperational = true;
+
+        //for future reading
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
+export default AppError;
