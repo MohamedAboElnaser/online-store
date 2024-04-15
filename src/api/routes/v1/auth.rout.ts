545902c36d699c8ed0da3d8b7a5d5067f84fd1ce
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2024-04-15 12:04:52 +0200

    validate `/register` endpoint

--- src/api/routes/v1/auth.rout.ts
+++ src/api/routes/v1/auth.rout.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { AuthController } from "../../controllers";
+import { validator } from "../../middlewares";
+import { authSchemas } from "../../../validation/schemas";
 
 const authRouter = Router();
 
+authRouter.use(validator(authSchemas));
 authRouter.route("/register").post(AuthController.register);
 
 export { authRouter };
