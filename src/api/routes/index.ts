a69b6b32461db4110d9b919b0747761f9b9e396e
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2024-04-16 19:01:26 +0200

    add demo endpoint

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import v1Router from "./v1";
 const mainRouter = Router();
-
+import passport from "passport";
 mainRouter.get("/api", (req, res) => {
     res.json({
         message: "api is working 🚀🚀",
@@ -10,6 +10,16 @@ mainRouter.get("/api", (req, res) => {
 
 //mount v1 router
 mainRouter.use("/api/v1", v1Router);
-
+//protected rout
+mainRouter.get(
+    "/protect",
+    passport.authenticate("jwt", { session: false }),
+    (req, res) =>
+        res.json({
+            status: "success",
+            message: "You are authenticated",
+            user: req.user,
+        })
+);
 export default mainRouter;
 export { mainRouter };
