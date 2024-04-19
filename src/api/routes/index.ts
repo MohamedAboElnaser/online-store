b0cb6ac7bd5e3a5d68fc866c475950bc3ed957b1
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2024-04-19 23:22:40 +0200

    remove the test endpoint

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import v1Router from "./v1";
-import { AuthHandler } from "../middlewares";
 const mainRouter = Router();
 
 mainRouter.get("/api", (req, res) => {
@@ -11,26 +10,6 @@ mainRouter.get("/api", (req, res) => {
 
 //mount v1 router
 mainRouter.use("/api/v1", v1Router);
-
-// Protected route
-mainRouter.get("/protect", AuthHandler.authenticate, (req, res) => {
-    res.json({
-        status: "success",
-        message: "You are authenticated",
-        user: req.user,
-    });
-});
-mainRouter.get(
-    "/protect-admin",
-    AuthHandler.authenticate,
-    AuthHandler.authorize("CUSTOMER"),
-    (req, res) => {
-        res.json({
-            status: "success",
-            message: "You are authenticated",
-        });
-    }
-);
 
 export default mainRouter;
 export { mainRouter };
