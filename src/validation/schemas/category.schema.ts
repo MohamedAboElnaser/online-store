c5481802416acf083a460ebb8e586745c32dd8ee
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2024-04-21 10:10:53 +0200

    Implement validation schema for update and delete category endpoints

--- src/validation/schemas/category.schema.ts
+++ src/validation/schemas/category.schema.ts
@@ -11,9 +11,34 @@ const createCategorySchema: TEndpointSchema[] = [
             .prefs({ abortEarly: false, stripUnknown: true }),
         target: "body",
     },
+    {
+        method: "DELETE",
+        target: "body",
+        schema: joi
+            .object({
+                ids: joi
+                    .array()
+                    .items(joi.string().guid({ version: "uuidv4" }))
+                    .required(),
+            })
+            .prefs({ abortEarly: false, stripUnknown: true }),
+    },
+];
+
+const updateCategorySchema: TEndpointSchema[] = [
+    {
+        method: "PATCH",
+        target: "body",
+        schema: joi
+            .object({
+                name: joi.string().min(3).optional(),
+            })
+            .prefs({ abortEarly: false, stripUnknown: true }),
+    },
 ];
 
 const endpoints: TValidationSchema = {
     "/": createCategorySchema,
+    "/:id": updateCategorySchema,
 };
 export default endpoints;
