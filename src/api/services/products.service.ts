b309a7de319a3548e43c01da34c106357fc566e0
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2024-05-05 17:36:21 +0300

    Implement ProductsService class
    - Implement createProduct method

--- src/api/services/products.service.ts
+++ src/api/services/products.service.ts
@@ -1,17 +1,63 @@
+import { DatabaseManager } from "../../../config";
 import { IProduct } from "product";
+import { AppError, CloudStorageService } from "../../util";
 
 class ProductsService {
     private constructor() {}
 
-    public static createProduct(
-        product: Partial<IProduct>,
-        images: Express.Multer.File[]
-    ) {
-        // first create record in database using text fields
+    public static async createProduct(
+        product: Pick<
+            IProduct,
+            "name" | "description" | "price" | "countInStock" | "categoryId"
+        >,
+        imagesFiles: Express.Multer.File[]
+    ): Promise<Omit<IProduct, "category">> {
+        let newProduct;
+        try {
+            /* 
+                First create record in database using text fields
+            */
+            newProduct = await DatabaseManager.getInstance().product.create({
+                data: {
+                    ...product,
+                },
+            });
+            /* 
+               Then save images to cloud storage naming it using the product id 
+            */
+            const imagesURLs = await CloudStorageService.uploadFiles(
+                imagesFiles,
+                newProduct.id
+            );
+            /* 
+                Then update the product record in the database with the image urls
+            */
+            newProduct = await DatabaseManager.getInstance().product.update({
+                where: {
+                    id: newProduct.id,
+                },
+                data: {
+                    images: imagesURLs,
+                },
+            });
 
-        // then save images to cloud storage naming it using the product id
-
-        // then update the product record in the database with the image urls
+            return newProduct;
+        } catch (err) {
+            //rollback
+            if (newProduct) {
+                await DatabaseManager.getInstance().product.delete({
+                    where: {
+                        id: newProduct.id,
+                    },
+                });
+                throw err;
+            } else {
+                throw new AppError(
+                    "Error happen while creating the product",
+                    500
+                );
+            }
+        }
     }
 }
 
