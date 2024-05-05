9368226a3c063aaa7f6cfe86c951d5932adddc51
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2024-05-05 12:44:59 +0300

    Handle multer error

--- src/api/middlewares/errorHandler.middleware.ts
+++ src/api/middlewares/errorHandler.middleware.ts
@@ -1,8 +1,7 @@
-import {Request,Response,NextFunction} from 'express'
-import { AppError } from '../../util'
+import { Request, Response, NextFunction } from "express";
+import { AppError } from "../../util";
 
-
-const developmentError = (err:AppError, req:Request,res:Response) => {
+const developmentError = (err: AppError, req: Request, res: Response) => {
     return res.status(err.statusCode).json({
         status: err.status,
         message: err.message,
@@ -11,7 +10,7 @@ const developmentError = (err:AppError, req:Request,res:Response) => {
     });
 };
 
-const productionError = (err:AppError, req:Request,res:Response) =>{
+const productionError = (err: AppError, req: Request, res: Response) => {
     //trusted error: send message to the client
     if (err.isOperational) {
         return res.status(err.statusCode).json({
@@ -27,15 +26,23 @@ const productionError = (err:AppError, req:Request,res:Response) =>{
     });
 };
 
-
-const globalErrorHandler = (err:AppError, req: Request, res: Response, next: NextFunction) => {
+const globalErrorHandler = (
+    err: AppError,
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
     err.statusCode = err.statusCode || 500;
     err.status = err.status || "error";
-
-    if (process.env.NODE_ENV === "production")
-         productionError(err , req, res);
-    else   developmentError(err , req, res);
-}
+    // custom multer error
+    if (err.name === "MulterError") {
+        err.statusCode = 400;
+        err.isOperational = true;
+        err.message = `Invalid field name or file count, please check the field name and the number of files you are sending.`;
+    }
+    if (process.env.NODE_ENV === "production") productionError(err, req, res);
+    else developmentError(err, req, res);
+};
 
 export default globalErrorHandler;
-export {globalErrorHandler}
+export { globalErrorHandler };
