20fb3ba71edfd39093625295a1a904f09fb2f463
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2024-05-06 18:09:54 +0300

    Implement deleteFolder method at CloudStorageService class
    - Enhance the naming of folder created using uploadFiles

--- src/util/cloud-storage-service.ts
+++ src/util/cloud-storage-service.ts
@@ -1,5 +1,4 @@
-import { v2 as cloudinary, UploadApiResponse } from "cloudinary";
-import exp from "constants";
+import { v2 as cloudinary } from "cloudinary";
 import AppError from "./appError";
 
 class CloudStorageService {
@@ -16,7 +15,7 @@ class CloudStorageService {
                     cloudinary.uploader
                         .upload_stream(
                             {
-                                folder: `$${folderName}`,
+                                folder: `${folderName}`,
                                 public_id: `0${idx}`,
                             },
                             (error, result) => {
@@ -38,6 +37,17 @@ class CloudStorageService {
                 "Error occurred while saving images to cloud storage",
                 500
             );
+        }
+    }
+
+    public static async deleteFolder(folderName: string) {
+        try {
+            const res = await cloudinary.api.delete_resources_by_prefix(
+                folderName
+            );
+            // console.log("Response from delete operation==>", res);
+        } catch (err) {
+            throw new AppError("Error occurred while deleting files", 500);
         }
     }
 }
