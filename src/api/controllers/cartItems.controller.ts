141a8b45ba4be1bdd118b6b6e9950de8911c32bd
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2024-05-16 01:59:53 +0300

    Implement Post /items endpoint
    - Add addItem method to the controller
    - Add the required middlewares needed

--- src/api/controllers/cartItems.controller.ts
+++ src/api/controllers/cartItems.controller.ts
@@ -1,11 +1,36 @@
 import { Request, Response, NextFunction } from "express";
 import { asyncHandler, notImplementedHandler } from "../../utils";
+import { CartItemsService } from "../services";
+import { IUser } from "user";
 
 class CartItemsController {
     private constructor() {}
     public static addItem = asyncHandler(
-        async (req: Request, res: Response, next: NextFunction) => {
-            notImplementedHandler(req, res);
+        async (
+            req: Request<any, any, { productId: string; quantity?: number }>,
+            res: Response,
+            next: NextFunction
+        ) => {
+            // console.log('req.user.Cart', (req.user as IUser).Cart[0]);
+            const {
+                id: customerId,
+                Cart: [{ id: cartId }],
+            } = req.user as IUser;
+            const { productId, quantity } = req.body;
+            const cartItem = await CartItemsService.addItemToCart(
+                cartId,
+                customerId,
+                productId,
+                quantity
+            );
+            res.status(201).json({
+                status: "success",
+                message: "Item added to cart",
+                data: {
+                    cartItem,
+                },
+            });
+            // notImplementedHandler(req, res);
         }
     );
 
--- src/api/routes/v1/cartItems.rout.ts
+++ src/api/routes/v1/cartItems.rout.ts
@@ -1,10 +1,18 @@
 import { Router } from "express";
 import { CartItemsController } from "../../controllers";
+import { AuthHandler, MulterService, validator } from "../../middlewares";
+import { cartItemsSchemas } from "../../../validation/schemas";
 
 const CartItemsRouter = Router();
 
 CartItemsRouter.route("/")
-    .post(CartItemsController.addItem)
+    .post(
+        AuthHandler.authenticate,
+        AuthHandler.authorize("CUSTOMER"),
+        MulterService.getNoneMethod(),
+        validator(cartItemsSchemas),
+        CartItemsController.addItem
+    )
     .get(CartItemsController.fetchItems)
     .delete(CartItemsController.deleteItems);
 
