85bd7a8af27c7fe9e9c168233d93e74937f7ff4f
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2024-06-11 09:23:57 +0300

    Implement CreateOrder Controller method at OrderController class

--- src/api/controllers/orders.controller.ts
+++ src/api/controllers/orders.controller.ts
@@ -1,11 +1,31 @@
+import { Request, Response, NextFunction } from "express";
 import { asyncHandler, notImplementedHandler } from "../../utils";
-
+import { OrdersService } from "../services";
+import { IUser } from "user";
 class OrderController {
     private constructor() {}
 
-    public static createOrder = asyncHandler(async (req, res, next) => {
-        notImplementedHandler(req, res);
-    });
+    public static createOrder = asyncHandler(
+        async (
+            req: Request<any, any, { cartItemsIds: string[] }>,
+            res: Response,
+            next: NextFunction
+        ) => {
+            const { id: customerId } = req.user as IUser;
+            const { cartItemsIds } = req.body;
+            const order = await OrdersService.createOrder(
+                cartItemsIds,
+                customerId
+            );
+            res.status(201).json({
+                status: "success",
+                message: "Order created successfully. Now you can pay for it",
+                data: {
+                    order,
+                },
+            });
+        }
+    );
 }
 
 export default OrderController;
