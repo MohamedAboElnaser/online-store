f79cc21fa2197958291afc99652358cfec6fb4e0
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2024-06-11 09:28:37 +0300

    Add required middleware stack to createOrder endpoint
    - Authenticate the endpoint
    - Authorize only the customer
    - Parsing body request for multi-part form data
    - Validate the body request using validator middleware

--- src/api/routes/v1/orders.rout.ts
+++ src/api/routes/v1/orders.rout.ts
@@ -1,9 +1,19 @@
 import { Router } from "express";
 import { OrdersController } from "../../controllers";
+import { validator, AuthHandler, MulterService } from "../../middlewares";
+import { orderSchemas } from "../../../validation/schemas";
 
 const ordersRouter = Router();
 
-ordersRouter.route("/").post(OrdersController.createOrder);
+ordersRouter
+    .route("/")
+    .post(
+        AuthHandler.authenticate,
+        AuthHandler.authorize("CUSTOMER"),
+        MulterService.getNoneMethod(),
+        validator(orderSchemas),
+        OrdersController.createOrder
+    );
 
 export default ordersRouter;
 export { ordersRouter };
