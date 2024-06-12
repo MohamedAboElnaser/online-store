95f8969b20a04a43f381ca94788c4e4405730479
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2024-06-13 00:01:36 +0300

    Refactor CreateCheckoutSession service

--- src/api/services/payment.service.ts
+++ src/api/services/payment.service.ts
@@ -14,6 +14,74 @@ class PaymentService {
          *  Create a checkout session using the StripeService.
          *  Return the payment url to the client.
          */
+        const { order, totalAmount } = await PaymentService.verifyOrderState(
+            orderId,
+            customer
+        );
+
+        const session = await StripeService.getInstance().createCheckoutSession(
+            {
+                name: `${customer.firstName}-${customer.lastName} Order`,
+                email: customer.email,
+                price: totalAmount,
+                quantity: 1,
+            },
+            "api/v1/payments/success",
+            "api/v1/payments/cancel"
+        );
+
+        // TODO
+        /**
+         * Implement Webhook endpoint to:
+         * => Update the order status and set the paidAt date after payment is successful.
+         * => Update number of items in stock after payment is successful.
+         * => Send an email to the customer after payment is successful.
+         * => Add a record to customerProducts table to mark the product as purchased
+         *    by the customer so he can add a review to the product.
+         * */
+        // TODO Delete below queries after implementing the webhook endpoint
+        /* 
+          Mark that customer bought these order items so he can add reviews to them
+          Update status of the order to COMPLETED
+        */
+        const upsertPromises = order.OrderItems.map((item) =>
+            DatabaseManager.getInstance().customerProduct.upsert({
+                where: {
+                    productId_customerId: {
+                        customerId: customer.id,
+                        productId: item.product.id,
+                    },
+                },
+                update: {},
+                create: {
+                    customerId: customer.id,
+                    productId: item.product.id,
+                },
+            })
+        );
+        await Promise.all(upsertPromises);
+        await DatabaseManager.getInstance().order.update({
+            where: {
+                id: orderId,
+            },
+            data: {
+                orderStatus: "COMPLETED",
+                paidAt: new Date(),
+            },
+        });
+
+        return session.url;
+    }
+
+    /**
+     * Verifies an order's existence, status and item availability in the store.
+     *
+     * @param {string} orderId - The unique identifier of the order.
+     * @param {IUser} customer - The customer object containing customer details.
+     * @returns {Promise<Object>} - A promise that resolves to the order details if the verification is successful.
+     * @throws {AppError} - Throws an error if the order does not exist, is already completed, or if any order items are out of stock.
+     */
+    public static async verifyOrderState(orderId: string, customer: IUser) {
         const order = await DatabaseManager.getInstance().order.findUnique({
             where: {
                 id: orderId,
@@ -76,59 +144,8 @@ class PaymentService {
             (acc, orderItem) => acc + orderItem.quantity * orderItem.unitPrice,
             0
         );
-        // Create a checkout session
-        const session = await StripeService.getInstance().createCheckoutSession(
-            {
-                name: `${customer.firstName}-${customer.lastName} Order`,
-                email: customer.email,
-                price: totalAmount,
-                quantity: 1,
-            },
-            "api/v1/payments/success",
-            "api/v1/payments/cancel"
-        );
-
-        // TODO
-        /**
-         * Implement Webhook endpoint to:
-         * => Update the order status and set the paidAt date after payment is successful.
-         * => Update number of items in stock after payment is successful.
-         * => Send an email to the customer after payment is successful.
-         * => Add a record to customerProducts table to mark the product as purchased
-         *    by the customer so he can add a review to the product.
-         * */
-        // TODO Delete this query after implementing the webhook
-        /* 
-          Mark that customer bought these order items so he can add reviews to them
-          Update status of the order to COMPLETED
-        */
-        const upsertPromises = order.OrderItems.map((item) =>
-            DatabaseManager.getInstance().customerProduct.upsert({
-                where: {
-                    productId_customerId: {
-                        customerId: customer.id,
-                        productId: item.product.id,
-                    },
-                },
-                update: {},
-                create: {
-                    customerId: customer.id,
-                    productId: item.product.id,
-                },
-            })
-        );
-        await Promise.all(upsertPromises);
-        await DatabaseManager.getInstance().order.update({
-            where: {
-                id: orderId,
-            },
-            data: {
-                orderStatus: "COMPLETED",
-                paidAt: new Date(),
-            },
-        });
-
-        return session.url;
+        // return the order details and total amount
+        return { order, totalAmount };
     }
 }
 
