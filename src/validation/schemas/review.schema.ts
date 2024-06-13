dad3e8ff234259dea041682998f226735e1574f3
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2024-06-13 21:20:37 +0300

    Implement validation schema for updating review endpoint

--- src/validation/schemas/review.schema.ts
+++ src/validation/schemas/review.schema.ts
@@ -1,7 +1,7 @@
 import * as joi from "joi";
 import { TEndpointSchema, TValidationSchema } from "validationSchema";
 
-const reviewSchema: TEndpointSchema[] = [
+const createReviewSchema: TEndpointSchema[] = [
     {
         method: "POST",
         target: "params",
@@ -23,8 +23,31 @@ const reviewSchema: TEndpointSchema[] = [
     },
 ];
 
+const updateReviewSchema: TEndpointSchema[] = [
+    {
+        method: "PATCH",
+        target: "params",
+        schema: joi
+            .object({
+                productId: joi.string().guid({ version: "uuidv4" }).required(),
+                reviewId: joi.string().guid({ version: "uuidv4" }).required(),
+            })
+            .prefs({ abortEarly: false, stripUnknown: true }),
+    },
+    {
+        method: "PATCH",
+        target: "body",
+        schema: joi
+            .object({
+                rating: joi.number().min(1).max(5),
+                comment: joi.string().min(3),
+            })
+            .prefs({ abortEarly: false, stripUnknown: true }),
+    },
+];
 const endpoints: TValidationSchema = {
-    "/": reviewSchema,
+    "/": createReviewSchema,
+    "/:id": updateReviewSchema,
 };
 
 export default endpoints;
