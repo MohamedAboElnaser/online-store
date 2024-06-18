9fbfd7547ca2ee8be76a4307640a466f36a684cb
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2024-06-18 22:41:36 +0300

    Implement test controller for webhook

--- src/api/controllers/payment.controller.ts
+++ src/api/controllers/payment.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
-import { asyncHandler, notImplementedHandler } from "../../utils";
+import { AppError, asyncHandler, notImplementedHandler } from "../../utils";
+import stripe from "stripe";
 import { PaymentService } from "../services";
 import { IUser } from "user";
 
@@ -20,6 +21,32 @@ class PaymentController {
                     paymentUrl,
                 },
             });
+        }
+    );
+
+    public static webhook = asyncHandler(
+        async (req: Request, res: Response) => {
+            const signature = req.headers["stripe-signature"];
+
+            let event;
+            try {
+                event = stripe.webhooks.constructEvent(
+                    req.body,
+                    signature,
+                    process.env.STRIPE_WEBHOOK_SECRET
+                );
+            } catch (err) {
+                throw new AppError(
+                    "Error happen while constructing the webhook event",
+                    400
+                );
+            }
+
+            if (event.type === "checkout.session.completed")
+                console.log("webhook endpoint works fine");
+            console.log("event.data.object", event.data.object);
+
+            res.status(200).json({ received: true });
         }
     );
 }
